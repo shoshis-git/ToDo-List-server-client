6f4686d75d1a78e2ede2818f71891e85b045edce
shoshis-git <sh0556735750@gmail.com>
2025-12-01 17:24:48 +0200

    change api

--- client/src/service.jsx
+++ client/src/service.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // הגדרת כתובת בסיס ל-API
 const apiClient = axios.create({
-  baseURL: "http://localhost:5126",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000",
 });
 
 // Interceptor ל-request: מוסיף את ה-Authorization header אוטומטית
