f911fbbb38c4fb760f586029f8101c7cce77da47
shoshis-git <sh0556735750@gmail.com>
2025-12-04 18:30:34 +0200

    changes last

--- client/src/service.jsx
+++ client/src/service.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://todo-list-server-client.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5126",
 });
 
 // ---- REQUEST ----
