d75b504af29b48eaa19a650a90e6a1415efb1fd2
shoshis-git <sh0556735750@gmail.com>
2025-12-04 22:14:33 +0200

    k

--- client/src/authService.jsx
+++ client/src/authService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL, // כתובת השרת שלך
+  baseURL: process.env.REACT_APP_API_URL, // כתובת השרת שלך
 });
 
 export default {
--- client/src/service.jsx
+++ client/src/service.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import './auth.css';
 // הגדרת axios עם כתובת הבסיס של ה-API
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL, // אם השרת שלך ברץ ב-5000, אחרת עדכן לפי הצורך
+  baseURL: process.env.REACT_APP_API_URL, // אם השרת שלך ברץ ב-5000, אחרת עדכן לפי הצורך
 });
 
 // פונקציות לתקשורת עם ה-API
@@ -14,7 +14,15 @@ export default {
   addTask: async(taskName) => apiClient.post('/items', { Name: taskName, IsComplete: false }),
 
   // קבלת כל המשימות
-  getTasks:async () => apiClient.get('/items'),
+  getTasks: async () => {
+  try {
+    const response = await apiClient.get('/items');
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    return []; // מחזיר מערך ריק במקרה של שגיאה
+  }
+},
 
   // עדכון סטטוס של משימה
   updateTask: async(id, name, isComplete) => apiClient.put(`/items/${id}`, { Name: name, IsComplete: isComplete }),
