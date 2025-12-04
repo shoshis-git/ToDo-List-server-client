1bca892b2755e7f4b0ee88d20fc3443e2201f123
shoshis-git <sh0556735750@gmail.com>
2025-12-04 17:56:06 +0200

    add dockerFile

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd5b306c9917ecd897c7c0625d2af5272e8b98a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651cf42200e9841a7c1ed42c7eb68c2faf650c7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
